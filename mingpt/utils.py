35f092da83f43089182e983e68fd3457edd7d0f1
Your Name <your.email@example.com>
2026-01-27 19:29:05 +0200

    improve setup_logging docstring for clarity

--- mingpt/utils.py
+++ mingpt/utils.py
@@ -18,7 +18,9 @@ def set_seed(seed):
     torch.cuda.manual_seed_all(seed)
 
 def setup_logging(config):
-    """ monotonous bookkeeping """
+    """ 
+     Initialize logging infrastructure by creating a working directory and saving configurations.
+     Creates a work directory and saves the command line arguments and configuration to files for experiment tracking and reproducibility."""
     work_dir = config.system.work_dir
     # create the work directory if it doesn't already exist
     os.makedirs(work_dir, exist_ok=True)
