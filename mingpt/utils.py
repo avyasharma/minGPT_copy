6efb3cdf56a5a3ac0701bb5035ae52476d145167
henry-feldhaus <176080339+henry-feldhaus@users.noreply.github.com>
2026-01-28 10:04:39 -0500

    added foo_function() to utils.py

--- mingpt/utils.py
+++ mingpt/utils.py
@@ -59,6 +59,9 @@ class CfgNode:
     def merge_from_dict(self, d):
         self.__dict__.update(d)
 
+    def foo_function(self):
+        pass
+
     def merge_from_args(self, args):
         """
         update the configuration from a list of strings that is expected
