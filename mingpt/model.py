d29818c48b401358df3c17f1f4896aa9f3c1b500
Juan Ortega <jcortega@andrew.cmu.edu>
2026-01-29 14:29:44 -0500

    line 62 in mingpt/model.py altered

--- mingpt/model.py
+++ mingpt/model.py
@@ -59,7 +59,7 @@ class CausalSelfAttention(nn.Module):
         v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
 
         # causal self-attention; Self-attend: (B, nh, T, hs) x (B, nh, hs, T) -> (B, nh, T, T)
-        att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+        att = (q @ k.transpose(-2, -1)) #Eliminated the normalization component of attention. (we normalize to prevent gradients from becoming too small during training)
         att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
         att = F.softmax(att, dim=-1)
         att = self.attn_dropout(att)
