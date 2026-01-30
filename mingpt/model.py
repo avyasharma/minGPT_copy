5ce3b4d9365a571c10345d1df85acb05ed07663a
Ashutosh Panda <ashutoshpanda2002@gmail.com>
2026-01-30 10:49:31 -0500

    Added TODOs

--- mingpt/model.py
+++ mingpt/model.py
@@ -8,6 +8,8 @@ https://github.com/openai/gpt-2/blob/master/src/model.py
 https://github.com/huggingface/transformers/blob/main/src/transformers/models/gpt2/modeling_gpt2.py
 """
 
+# TODO: Integrate other novel experimental Activation Functions...
+
 import math
 
 import torch
