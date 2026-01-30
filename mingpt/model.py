5e6c1804a5d94f9dc65057e77d2c8f6f9facf8ff
buffett0323 <philip910323@gmail.com>
2026-01-30 11:31:17 -0500

    Add line for main branch

--- mingpt/model.py
+++ mingpt/model.py
@@ -7,7 +7,7 @@ https://github.com/openai/gpt-2/blob/master/src/model.py
 2) huggingface/transformers PyTorch implementation:
 https://github.com/huggingface/transformers/blob/main/src/transformers/models/gpt2/modeling_gpt2.py
 """
-
+# Line for merge conflict in main
 import math
 
 import torch
