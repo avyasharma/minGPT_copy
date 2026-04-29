39e7ffeec21c5ffb2767f0a87de1648175c1718e
Yunqi <yunqi@Yunqis-MacBook-Air-5.local>
2026-04-29 14:07:50 -0400

    changes

--- mingpt/model.py
+++ mingpt/model.py
@@ -8,6 +8,7 @@ https://github.com/openai/gpt-2/blob/master/src/model.py
 https://github.com/huggingface/transformers/blob/main/src/transformers/models/gpt2/modeling_gpt2.py
 """
 
+## changing
 import math
 
 import torch
